d52bbfbb5d6bc480f97d170071e5ce5dbfedc46f
zhe-hong-Lin <zhehome1021@gmail.com>
2021-09-13 09:57:38 +0800

    population

--- app/Models/Population.php
+++ app/Models/Population.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Population extends Model
 {
     use HasFactory;
+    
     protected $table = 'population';
     protected $fillable = ['district', 'total'];
+
+    // turn off both 
+    public $timestamps = false;
+
+    // turn off only updated_at
+    const UPDATED_AT = false;
 }
